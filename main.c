e39405ef42206216e6d485832dd61976ad5efdf3
Jo <admin@admins-MacBook-Pro.local>
2026-01-23 18:42:14 +0100

    parser finsished

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: admin <admin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 17:46:48 by admin             #+#    #+#             */
-/*   Updated: 2026/01/21 13:57:20 by admin            ###   ########.fr       */
+/*   Updated: 2026/01/23 18:42:05 by admin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,15 @@
 
 int	main(int argc, char **argv)
 {
-	t_list	stack_a;
+	t_stack	*stack_a;
 
 	if (argc == 1) // case where no argument is passed at execution time
 		return ;
 	stack_a = parser(argc, argv);
+	if (!stack_a)
+	{
+		write(2, "Error\n", 6);
+		return ;
+	}
 	turk_sort(stack_a);
 }
