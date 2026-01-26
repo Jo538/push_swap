254d15cc3bdaad22ab0cea041b677ff9edf48001
Josephine Chartier <jchartie@f4r5s16.clusters.42paris.fr>
2026-01-26 14:49:25 +0100

    find_target

--- algo/utils_1.c
+++ algo/utils_1.c
@@ -6,7 +6,7 @@
 /*   By: jchartie <jchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 12:13:37 by jchartie          #+#    #+#             */
-/*   Updated: 2026/01/26 13:49:37 by jchartie         ###   ########.fr       */
+/*   Updated: 2026/01/26 14:46:51 by jchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,31 @@ int	is_sorted(t_stack *stack)
 	if (count >= 1 && (temp -> number > stack-> head -> number))
 		return (0);
 	return (1);
+}
+
+int	find_target(int num, t_stack *stack)
+{
+	t_list	*temp;
+	int		b_tar;
+	int		max;
+	int		found;
+
+	temp = stack -> head;
+	max = temp -> number;
+	found = 0;
+
+	while (temp)
+	{
+		if (temp -> number > max)
+			max = temp -> number;
+		if ((temp -> number < num) && (!found || (temp -> number > b_tar)))
+		{
+			b_tar = temp -> number;
+			found = 1;
+		}
+		temp = temp -> next;
+	}
+	if (!found)
+		return (max);
+	return (b_tar);
 }
--- tests/tests_algo.c
+++ tests/tests_algo.c
@@ -6,7 +6,7 @@
 /*   By: jchartie <jchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 12:20:23 by jchartie          #+#    #+#             */
-/*   Updated: 2026/01/26 13:50:14 by jchartie         ###   ########.fr       */
+/*   Updated: 2026/01/26 14:49:02 by jchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,27 @@ int main(void)
 		printf("%s\n", "The list is sorted");
 
 	#endif
+
+
+	
+	#ifdef FIND_TARGET_TEST
+
+	int num = -10;
+	long list_of_longs[] = {-3, -1, 0, -2};
+	int len = 4;
+	long *array_of_longs = ft_calloc(len, sizeof(long));
+	int i = 0;
+	while (i < len)
+	{
+		array_of_longs[i] = list_of_longs[i];
+		i++;
+	}
+	t_stack *stack = create_stack(array_of_longs, len);
+
+	int j = find_target(num, stack);
+
+	printf("The target for %d is %d\n", num, j);
+	
+	#endif
+
 }
